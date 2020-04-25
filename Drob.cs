05a8c062de5fd457132a974cac21cce677cea7bd
starosta-coder <54850884+starosta-coder@users.noreply.github.com>
2020-04-25 19:09:57 +0300

    Add files via upload
    Variant 13

---
+++ Drob.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Frection
+{
+    class Drob
+    {
+        private double num;
+        private double denom;
+        private double result;
+
+        public double Num { get => num; private set => num = value; }
+        public double Denom { get => denom; private set => denom = value; }
+
+
+        public Drob(double num, double denum)
+        {
+            Num = num;
+            Denom = denum;
+            result = Math.Round(num / denum,2);
+        }
+
+        public void Exponentiation(double num)
+        {
+            num = Math.Pow(num, num);
+            Denom = Math.Pow(Denom, num);
+        }
+
+        public static bool operator ==(Drob temp1, Drob temp2)
+        {
+            return temp1.result.Equals(temp2.result);
+        }
+
+        public static bool operator !=(Drob temp1, Drob temp2)
+        {
+            return !temp1.result.Equals(temp2.result);
+        }
+
+        public static bool operator >(Drob temp1, Drob temp2)
+        {
+            return temp1.result > temp2.result;
+        }
+
+        public static bool operator <(Drob temp1, Drob temp2)
+        {
+            return temp1.result < temp2.result;
+        }
+
+        public static bool operator >=(Drob temp1, Drob temp2)
+        {
+            return temp1.result >= temp2.result;
+        }
+
+        public static bool operator <=(Drob temp1, Drob temp2)
+        {
+            return temp1.result <= temp2.result;
+        }
+
+        public static Drob operator +(Drob temp1, Drob temp2)
+        {
+            return new Drob(temp1.num * temp2.Denom + temp1.Denom * temp2.num, temp1.Denom * temp2.Denom);
+        }
+
+        public static Drob operator -(Drob temp1, Drob temp2)
+        {
+            return new Drob(temp1.num * temp2.Denom - temp1.Denom * temp2.num, temp1.Denom * temp2.Denom);
+        }
+
+        public static Drob operator *(Drob temp1, Drob temp2)
+        {
+            return new Drob(temp1.num * temp2.num, temp1.Denom * temp2.Denom);
+        }
+
+        public static Drob operator /(Drob temp1, Drob temp2)
+        {
+            return new Drob(temp1.num * temp2.Denom, temp1.Denom * temp2.num);
+        }
+
+        public static Drob operator ^(Drob temp1, double x)
+        {
+            return new Drob(Math.Pow(temp1.num, x), Math.Pow(temp1.Denom, x));
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", num, Denom);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Drob Drob &&
+                   num == Drob.num &&
+                   Denom == Drob.Denom &&
+                   result == Drob.result;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 471222432;
+            hashCode = hashCode * -1521134295 + num.GetHashCode();
+            hashCode = hashCode * -1521134295 + Denom.GetHashCode();
+            hashCode = hashCode * -1521134295 + result.GetHashCode();
+
+            return hashCode;
+        }
+
+        public static Drob Down(Drob drob)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (drob.Num % i == 0 && drob.Denom % i == 0)
+                    {
+                        drob.Num /= i;
+                        drob.Denom /= i;
+                    }
+                }
+            }
+            
+            return drob;
+        } 
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Windows;
+
+namespace WPF_Frection
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonRandom_Click(object sender, RoutedEventArgs e)
+        {
+            Drob[] drobs = new Drob[4];
+            for (int i = 0; i < 4; i++)
+            {
+                Random random = new Random();
+                drobs[i] = new Drob(random.Next(1, (i + 3) * 4), random.Next(1, (i + 1) * 5));
+                Drob.Down(drobs[i]);
+            }
+
+            textBoxFirstNum.Text = drobs[0].Num.ToString();
+            textBoxFirstDenom.Text = drobs[0].Denom.ToString();
+
+            textBoxSecondNum.Text = drobs[1].Num.ToString();
+            textBoxSecondDenom.Text = drobs[1].Denom.ToString();
+
+            textBoxThirdNum.Text = drobs[2].Num.ToString();
+            textBoxThirdDenom.Text = drobs[2].Denom.ToString();
+
+            textBoxFourthNum.Text = drobs[3].Num.ToString();
+            textBoxFourthDenom.Text = drobs[3].Denom.ToString();
+
+            labelResultFirst.FontSize = 16;
+            labelResultFirst.Text = "Дробь№1\n" + drobs[0].ToString();
+            labelResultSecond.FontSize = 16;
+            labelResultSecond.Text = "Дробь№2\n" + drobs[1].ToString();
+            labelResultThird.FontSize = 16;
+            labelResultThird.Text = "Дробь№3\n" + drobs[2].ToString();
+            labelResultFourth.FontSize = 16;
+            labelResultFourth.Text = "Дробь№4\n" + drobs[3].ToString();
+        }
+
+        private void result1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Drob drob1 = new Drob(double.Parse(textBoxFirstNum.Text), double.Parse(textBoxFirstDenom.Text));
+                Drob drob2 = new Drob(double.Parse(textBoxSecondNum.Text), double.Parse(textBoxSecondDenom.Text));
+                Drob drob3 = new Drob(double.Parse(textBoxThirdNum.Text), double.Parse(textBoxThirdDenom.Text));
+                Drob drob4 = new Drob(double.Parse(textBoxFourthNum.Text), double.Parse(textBoxFourthDenom.Text));
+
+                Drob result = (drob1 + drob2) * (drob3 - drob4);
+                textBlockResul.Text = Drob.Down(result).ToString();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+
+        private void result2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Drob drob1 = new Drob(double.Parse(textBoxFirstNum.Text), double.Parse(textBoxFirstDenom.Text));
+                Drob drob2 = new Drob(double.Parse(textBoxSecondNum.Text), double.Parse(textBoxSecondDenom.Text));
+                Drob drob3 = new Drob(double.Parse(textBoxThirdNum.Text), double.Parse(textBoxThirdDenom.Text));
+                Drob drob4 = new Drob(double.Parse(textBoxFourthNum.Text), double.Parse(textBoxFourthDenom.Text));
+
+                Drob result = (drob4 + drob2) / (drob1 - drob3);
+                textBlockResul.Text = Drob.Down(result).ToString();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+
+        private void result3_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Drob[] drobs = new Drob[4]
+                {
+            new Drob(double.Parse(textBoxFirstNum.Text), double.Parse(textBoxFirstDenom.Text)),
+            new Drob(double.Parse(textBoxSecondNum.Text), double.Parse(textBoxSecondDenom.Text)),
+            new Drob(double.Parse(textBoxThirdNum.Text), double.Parse(textBoxThirdDenom.Text)),
+            new Drob(double.Parse(textBoxFourthNum.Text), double.Parse(textBoxFourthDenom.Text))
+                };
+                for (int i = 0; i < drobs.Length; i++)
+                {
+                    for (int j = i + 1; j < drobs.Length; j++)
+                    {
+                        if (drobs[j] < drobs[i])
+                        {
+                            Drob temp = drobs[j];
+                            drobs[j] = drobs[i];
+                            drobs[i] = temp;
+                        }
+                    }
+                }
+                textBlockResul.FontSize = 15;
+                textBlockResul.Text = drobs[0].ToString() + "<" +
+                    drobs[1].ToString() + "<" +
+                    drobs[2].ToString() + "<" +
+                    drobs[3].ToString();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void own_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Drob drob1 = new Drob(double.Parse(textBoxFirstNum.Text), double.Parse(textBoxFirstDenom.Text));
+                Drob drob2 = new Drob(double.Parse(textBoxSecondNum.Text), double.Parse(textBoxSecondDenom.Text));
+                Drob drob3 = new Drob(double.Parse(textBoxThirdNum.Text), double.Parse(textBoxThirdDenom.Text));
+                Drob drob4 = new Drob(double.Parse(textBoxFourthNum.Text), double.Parse(textBoxFourthDenom.Text));
+
+                labelResultFirst.FontSize = 16;
+                labelResultFirst.Text = "Дробь№1\n" + drob1.ToString();
+                labelResultSecond.FontSize = 16;
+                labelResultSecond.Text = "Дробь№2\n" + drob2.ToString();
+                labelResultThird.FontSize = 16;
+                labelResultThird.Text = "Дробь№3\n" + drob3.ToString();
+                labelResultFourth.FontSize = 16;
+                labelResultFourth.Text = "Дробь№4\n" + drob4.ToString();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+        }
+    }
+}
